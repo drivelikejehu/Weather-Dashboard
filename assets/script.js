82f591ad18ca9cec4095de3f81a291d75f2a9f81
John Kang <pollyfactor@hotmail.com>
2020-03-17 19:50:49 -0400

    updated script.js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+$(document).ready(function(){
+  console.log("hhh");
+
+    $("#searchBtn").on("click", function(event){
+      event.preventDefault();
+      
+      var cityInput = $("#searchInput").val();
+      console.log(cityInput);
+
+
+
+    });
+    
+  });
