97634ec4fcb0994256a3b19e58aa738f7a28f81d
John Kang <pollyfactor@hotmail.com>
2020-03-17 21:38:35 -0400

    updated temperature information

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
 
+  // function cityButtons() {
+  //   $("#city-buttons").text("HJHJHJ");
+  // }
+
     $("#searchBtn").on("click", function(event){
       event.preventDefault();
       $(".card").show();
+      $("#five-day-forecast").show();
       
       var cityInput = $("#searchInput").val();
       console.log(cityInput);
@@ -13,14 +18,27 @@ $(document).ready(function(){
         url: queryURL,
         method: "GET"
       }).then(function(response) {
-        console.log(response.name);
+        console.log(response.main.humidity);
 
-        $(".card-city").html("<h5>" + response.name + "</h5>");
-        $(".")
-        $(".")
-        $(".")
-        $(".")
+        function tempConversion() {
+          var temp = (response.main.temp - 273.15) * 1.80 + 32;
+          var tempFarhenheit = Math.floor(temp);
+          return tempFarhenheit;
+
+        }
+        // (results[i].main.temp - 273.15) * 1.80 + 32;
+        //   let tempF = Math.floor(temp);
+  
+
+        $(".card-city").html("<h3>" + response.name + "</h3>");
+        $(".card-temp").text("Temperature: " + tempConversion() + " °F")
+        $(".card-humid").text("Humidity: " + response.main.humidity);
+        $(".card-wind").text("Wind Speed: " + response.wind.speed + " MPH");
+        $(".card-uv")
       });
+
+      
+      // cityButtons();
       // <h5 class="card-city"></h5>
       // <div class="card-temp">Temperature: </div>
       // <div class="card-humid">Humidity: </div>
