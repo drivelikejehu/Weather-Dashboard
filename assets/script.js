a40fd6d063d2e0ce1e013642762628620af4ce77
John Kang <pollyfactor@hotmail.com>
2020-03-17 22:09:02 -0400

    updated weather conditions

--- assets/script.js
+++ assets/script.js
@@ -10,29 +10,36 @@ $(document).ready(function(){
       $("#five-day-forecast").show();
       
       var cityInput = $("#searchInput").val();
-      console.log(cityInput);
+      var apiKey = "&appid=caf1fb63b24af4c69a64677a2a7ad5ae";
 
-      var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=caf1fb63b24af4c69a64677a2a7ad5ae";
+      var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + apiKey;
+      var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + apiKey;
 
       $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
-        console.log(response.main.humidity);
 
         function tempConversion() {
           var temp = (response.main.temp - 273.15) * 1.80 + 32;
           var tempFarhenheit = Math.floor(temp);
           return tempFarhenheit;
         }
-  
 
         $(".card-city").html("<h3>" + response.name + "</h3>");
         $(".card-temp").text("Temperature: " + tempConversion() + " °F")
         $(".card-humid").text("Humidity: " + response.main.humidity);
         $(".card-wind").text("Wind Speed: " + response.wind.speed + " MPH");
-        $(".card-uv")
+        // $(".card-uv")
       });
+
+      $.ajax({
+        url: fiveDayURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+      
+    });
 
       
       // cityButtons();
