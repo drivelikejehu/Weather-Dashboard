87b51e809290b10e5d638b0b2313c24112d3ff3b
John Kang <pollyfactor@hotmail.com>
2020-03-17 20:09:09 -0400

    updated with ajax

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
-  console.log("hhh");
 
     $("#searchBtn").on("click", function(event){
       event.preventDefault();
       
       var cityInput = $("#searchInput").val();
       console.log(cityInput);
+
+      var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=caf1fb63b24af4c69a64677a2a7ad5ae";
+
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+      });
 
 
 
