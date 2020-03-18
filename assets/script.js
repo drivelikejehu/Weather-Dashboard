3acf1c575ae94749f4c6625abc7da905d1131dee
John Kang <pollyfactor@hotmail.com>
2020-03-17 22:37:49 -0400

    added date next to city

--- assets/script.js
+++ assets/script.js
@@ -4,14 +4,22 @@ $(document).ready(function(){
   //   $("#city-buttons").text("HJHJHJ");
   // }
 
+  // https://tecadmin.net/get-current-date-time-javascript/
+  // http://jsfiddle.net/nCE9u/
+
+  var d = new Date();
+  var month = d.getMonth()+1;
+  var day = d.getDate();
+  var date = "(" + ((''+month).length<2 ? '0' : '') + month + '/' +
+    ((''+day).length<2 ? '0' : '') + day + '/' + d.getFullYear() + ")";
+
     $("#searchBtn").on("click", function(event){
       event.preventDefault();
       $(".card").show();
       $("#five-day-forecast").show();
-      
+
       var cityInput = $("#searchInput").val();
       var apiKey = "&appid=caf1fb63b24af4c69a64677a2a7ad5ae";
-
       var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + apiKey;
       var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + apiKey;
 
@@ -20,13 +28,16 @@ $(document).ready(function(){
         method: "GET"
       }).then(function(response) {
 
+        console.log(response);
+
         function tempConversion() {
           var temp = (response.main.temp - 273.15) * 1.80 + 32;
           var tempFarhenheit = Math.floor(temp);
           return tempFarhenheit;
         }
 
-        $(".card-city").html("<h3>" + response.name + "</h3>");
+        $(".card-city").html("<h3>" + response.name + " " + date + "</h3>");
+        // $(".card-city").text(date.toLocaleDateString('en-US'));
         $(".card-temp").text("Temperature: " + tempConversion() + " °F")
         $(".card-humid").text("Humidity: " + response.main.humidity);
         $(".card-wind").text("Wind Speed: " + response.wind.speed + " MPH");
@@ -37,7 +48,7 @@ $(document).ready(function(){
         url: fiveDayURL,
         method: "GET"
       }).then(function(response) {
-        console.log(response);
+        console.log(response.list[0]);
       
     });
 
